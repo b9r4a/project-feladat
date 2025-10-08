cd67a0386edb47ff294fa2673ff90977cf412476
PettyanBotond <118384806+PettyanBotond@users.noreply.github.com>
2025-10-08 12:26:01 +0200

    Modal befejezése & Id létrehozása
    yay

--- java.js
+++ java.js
@@ -25,6 +25,41 @@ window.onclick = function(event) {
   }
 }
 
-function kattintas() {
-    
+function gyerekkattintas() {
+    gyerekmodal.style.display = "block";
+    span.onclick = function() {
+      gyerekmodal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if(event.target == gyerekmodal) {
+        gyerekmodal.style.display = "none";
+      }
+    }
 }
+function felnottkattintas() {
+    felnottmodal.style.display = "block";
+    span.onclick = function() {
+      felnottmodal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if(event.target == felnottmodal) {
+        felnottmodal.style.display = "none";
+      }
+    }
+}
+
+/*btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}*/
