577b44ef0e5f1db7c58d502db8e8fd6200ec4e7d
b9r4a <166108420+b9r4a@users.noreply.github.com>
2025-10-11 15:33:42 +0200

    js javitása

--- java.js
+++ java.js
@@ -8,25 +8,7 @@ let btn = document.getElementById("myBtn");
 
 let span = document.getElementsByClassName("close")[0];
 
-//felnott
-let indexArray = []
 
-for (let i = 0; i < 10; i++) {
-  indexArray.push(i)
-
-}
-
-//gyerek
-let indexArray2 = []
-
-for (let i = 0; i < 10; i++) {
-  indexArray2.push(i)
-}
-
-function randomArray() {
-  indexArray.sort(() => Math.random() - 0.5);
-  console.log(indexArray);
-}
 
 
 btn.onclick = function() {
@@ -68,47 +50,5 @@ function felnottkattintas() {
     }
 }
 
-//felnott
-window.addEventListener("load", () => {
-  alert("ok")
-  randomArray();
-  fetch("questions.json")
-    .then(json => json.json())
-    .then(json => {writeJson(json)});
-})
 
-function writeJson(json){
-  indexArray.forEach(i => {
-    const item = json[i]
-    console.log(item.question)
-    document.getElementById("datas").innerHTML = `<h3>${item.question}</h3>`;
-    
-    item.answers.forEach(element => {
-      console.log(`Válasz: + ${element.text} - ${element.points}`)
-      document.getElementById("datas").innerHTML = `<input type="radio" name="${item.question}" value="${element.points}">${element.text}<br>`;
-      
-
-    });
-  });
-}
-
-
-//gyerek
-document.getElementById("button2").addEventListener("click", () => {
-  randomArray();
-  fetch("questions2.json")
-    .then(json => json.json())
-    .then(json => {writeJson(json)});
-})
-
-function writeJson(json){
-  indexArray2.forEach(i => {
-    const items = json[i]
-    console.log(items.question)
-    items.answers.forEach(element => {
-      console.log(`Válasz: + ${element.text} - ${element.points}`)
-
-    });
-  });
-}
 
---
+++ kwiz.js
@@ -0,0 +1,32 @@
+let indexArray = []
+
+function randomArray() {
+  indexArray.sort(() => Math.random() - 0.5);
+  console.log(indexArray);
+}
+
+window.addEventListener("load", () => {
+for (let i = 0; i < 10; i++) {
+  indexArray.push(i)
+
+}
+  randomArray();
+  fetch("questions.json")
+    .then(json => json.json())
+    .then(json => {writeJson(json)});
+})
+
+function writeJson(json){
+  indexArray.forEach(i => {
+    const item = json[i]
+    console.log(item.question)
+    document.getElementById("datas").innerHTML += `<h3>${item.question}</h3>`;
+    
+    item.answers.forEach(element => {
+      console.log(`Válasz: + ${element.text} - ${element.points}`)
+      document.getElementById("datas").innerHTML += `<input type="radio" name="${item.question}" value="${element.points}">${element.text}<br>`;
+      
+
+    });
+  });
+}
